3ac87c8adf24c9f980a1021c7ce2e04c6036221d
dhe <dhe@instar.cz>
2014-09-01 09:04:45 +0200

    volne nanuky ziskavany z view

--- app/model/SkladRepository.php
+++ app/model/SkladRepository.php
@@ -12,18 +12,11 @@ class SkladRepository extends Repository
   protected $table = 'sklad';
 
   /**
+   * Počty nanuků v mražáku.
    * @return \Nette\Database\Table\Selection
    */
   public function inventura()
   {
-    return $this->database->query(
-      'SELECT nanuky.nazev, count(nanuky_id) AS pocet,
-      (SELECT cena_prodej FROM sklad s2 WHERE s1.nanuky_id = s2.nanuky_id AND s2.kupci_id IS NULL AND s2.cena_prodej > 0 ORDER BY id LIMIT 1) AS cena
-      FROM sklad s1, nanuky
-      WHERE s1.nanuky_id = nanuky.id
-      AND s1.kupci_id IS NULL
-      AND s1.cena_prodej > 0
-      GROUP BY nanuky_id;'
-    );
+    return $this->database->table('volne_nanuky')->select('nanuky_id, nazev, count(nanuky_id) AS pocet')->group('nanuky_id');
   }
 }

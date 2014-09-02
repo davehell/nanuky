e570219881ad521319d91e541d75b5034569dcf4
dhe <dhe@instar.cz>
2014-09-02 08:09:00 +0200

    do sql skriptu doplneni dalsi kupci

--- app/db/nanuky.sql
+++ app/db/nanuky.sql
@@ -8,10 +8,26 @@ CREATE TABLE "kupci" (
   PRIMARY KEY ("jmeno")
 );
 
-INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('dhe',	'0',	'0');
-INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('jbo',	10,	'0');
-INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('tmo',	11,	'0');
-INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('jsv',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('DHE',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('JBO',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('TMO',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('JSV',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('AKI',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('ARA',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('JHB',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('JHR',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('JLA',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('KPE',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('LBU',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('MHA',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('MKR',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('MMA',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('MPI',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('MSA',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('PVR',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('RKO',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('VMA',	'0',	'0');
+INSERT INTO "kupci" ("jmeno", "dluh", "zaplaceno") VALUES ('VNE',	'0',	'0');
 
 DROP TABLE IF EXISTS "mrazak";
 CREATE TABLE "mrazak" (
@@ -24,55 +40,6 @@ CREATE TABLE "mrazak" (
   FOREIGN KEY ("kupec") REFERENCES "kupci" ("jmeno") ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (1,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (2,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (3,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (4,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (5,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (6,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (7,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (8,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (9,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (10,	4,	NULL,	'10.400000000000000355',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (11,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (12,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (13,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (14,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (15,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (16,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (17,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (18,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (19,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (20,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (21,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (22,	3,	NULL,	'14.5',	15);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (23,	1,	'tmo',	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (24,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (25,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (26,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (27,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (28,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (29,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (30,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (31,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (32,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (33,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (34,	1,	NULL,	'10.300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (35,	5,	'jbo',	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (36,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (37,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (38,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (39,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (40,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (41,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (42,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (43,	5,	NULL,	'9.6999999999999992895',	10);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (44,	2,	NULL,	'8.8300000000000000711',	11);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (45,	6,	NULL,	'11.5',	12);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (46,	6,	NULL,	'11.5',	12);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (47,	6,	NULL,	'11.5',	12);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (48,	6,	NULL,	'11.5',	12);
-INSERT INTO "mrazak" ("id", "nanuky_id", "kupec", "cena_nakup", "cena_prodej") VALUES (49,	6,	NULL,	'11.5',	12);
 
 DROP TABLE IF EXISTS "nanuky";
 CREATE TABLE "nanuky" (
@@ -88,12 +55,14 @@ INSERT INTO "nanuky" ("id", "nazev", "ean", "baleni") VALUES (3,	'Mrož jahoda -
 INSERT INTO "nanuky" ("id", "nazev", "ean", "baleni") VALUES (4,	'Ruská zmrzlina',	'4',	10);
 INSERT INTO "nanuky" ("id", "nazev", "ean", "baleni") VALUES (5,	'Pegas - vanilka',	'5',	9);
 INSERT INTO "nanuky" ("id", "nazev", "ean", "baleni") VALUES (6,	'Míša',	'6',	16);
+INSERT INTO "nanuky" ("id", "nazev", "ean", "baleni") VALUES (7,	'Mrož - dřeň jahoda',	'7',	12);
+INSERT INTO "nanuky" ("id", "nazev", "ean", "baleni") VALUES (8,	'Mrož - dřeň malina',	'8',	12);
+INSERT INTO "nanuky" ("id", "nazev", "ean", "baleni") VALUES (9,	'Pegas - kokos',	'9',	9);
 
 DROP TABLE IF EXISTS "sqlite_sequence";
 CREATE TABLE sqlite_sequence(name,seq);
 
-INSERT INTO "sqlite_sequence" ("name", "seq") VALUES ('nanuky',	'6');
-INSERT INTO "sqlite_sequence" ("name", "seq") VALUES ('mrazak',	'49');
+INSERT INTO "sqlite_sequence" ("name", "seq") VALUES ('nanuky',	'9');
 
 DROP VIEW IF EXISTS "volne_nanuky";
 CREATE TABLE "volne_nanuky" ("id" integer, "nanuky_id" integer, "nazev" text, "cena" integer);

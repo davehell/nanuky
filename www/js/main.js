a449aad6d59bbf3769b1c76c094deaf9c3cd647f
dhe <dhe@instar.cz>
2014-12-12 10:38:38 +0100

    oprava splaceni dluhu - aby slo tlacitko mackat opakovane

--- www/js/main.js
+++ www/js/main.js
@@ -1,13 +1,27 @@
+$(function () {
+$.nette.ext({
+    load: function() {
+      /**
+       * Splacení dluhu zákazníka.
+       */
+      $('button.dluh').click(function (event) {
+        var url = $(this).data('url');
+        var jmeno = $(this).data('jmeno');
+        var castka = $( "input#dluh-" + jmeno ).val();
+
+        if(!castka) return;
+        $.nette.ajax({url: url, data: {"castka": castka}}, event).done(function() {
+        });
+      });
+    }
+});
 $.nette.init();
-
-$( "#frm-nanukForm-cena" ).focus();
-
-$( "#frm-nanukForm-cena" ).change(function() {
-  $( "#frm-nanukForm-pocet" ).val("");
-  $( "#frm-nanukForm-cena_nakup" ).val("");
-  $( "#frm-nanukForm-cena_prodej" ).val("");
 });
 
+/**
+ * Výpočet prodejní ceny jednoho nanuku na základě ceny celého balení.
+ * @param  {int} pocetKusu počet kusů v balení
+ */
 function nastavCeny(pocetKusu) {
   var cenaBaleni = parseFloat(($( "#frm-nanukForm-cena" ).val()).replace(/,/g, "."));
   var cenaNakup = (cenaBaleni / pocetKusu).toFixed(2);
@@ -16,24 +30,34 @@ function nastavCeny(pocetKusu) {
   $( "#frm-nanukForm-cena_prodej" ).val(cenaProdej);
 }
 
+/**
+ * Nastavení focusu na políčko s cenou balení.
+ */
+$( "#frm-nanukForm-cena" ).focus();
+
+/**
+ * Při změně ceny balení se vymažou ostatní políčka.
+ */
+$( "#frm-nanukForm-cena" ).change(function() {
+  $( "#frm-nanukForm-pocet" ).val("");
+  $( "#frm-nanukForm-cena_nakup" ).val("");
+  $( "#frm-nanukForm-cena_prodej" ).val("");
+});
+
+/**
+ * Při výběru druhu nanuku se aktualizuje prodejní cena.
+ */
 $( "#frm-nanukForm-nanuky_id" ).change(function() {
   var pocetKusu = velikostiBaleni[this.value];
   $( "#frm-nanukForm-pocet" ).val(pocetKusu);
   nastavCeny(pocetKusu);
 });
 
+/**
+ * Při změně počtu kusů v balení se aktualizuje prodejní cena.
+ */
 $( "#frm-nanukForm-pocet" ).change(function() {
   var pocetKusu = $( this ).val();
   nastavCeny(pocetKusu);
-});
-
-$('button.dluh').click(function (event) {
-  var url = $(this).data('url');
-  var jmeno = $(this).data('jmeno');
-  var castka = $( "input#dluh-" + jmeno ).val();
-
-  if(!castka) return;
-  $.nette.ajax({url: url, data: {"castka": castka}}, event).done(function() {
-  });
 });
 
